24792f9125d230c66b71aeedcfdfd8ab5731b39a
Rukhiddin <Ekonomistmiradhamov@gmail.com>
2024-11-28 13:46:32 +0500

    Fake Homework

--- src/Main.kt
+++ src/Main.kt
@@ -1,18 +1,5 @@
 fun main() {
 
-    /*This is
-    * block
-    * Comment*/
-
-
-    println("Hello Kotlin!")
-    println("Lesson")
-
-//    Here print functions
-
-    println()
-
-//    println("Name: Asadbek")
-    print("name Rukhiddin")
+    println("Homework was done!")
 
 }
