38797d87701bd5ea45ec93602b9d44d661529ddb
Rukhiddin <Ekonomistmiradhamov@gmail.com>
2024-12-06 18:00:33 +0500

    Homework 3

--- src/lesson4/lesson4.kt
+++ src/lesson4/lesson4.kt
@@ -2,12 +2,22 @@ package lesson4
 
 fun main() {
     val userAge = readln().toInt()
-    if (userAge >= MAJORITY_OF_AGE){
-        println("Show special content")
+    val resultText = if (userAge >= MAJORITY_OF_AGE){
+        "Show special content"
+    } else if (userAge == 16 || userAge ==17) {
+       "Show limited content"
     }
-    if (userAge < MAJORITY_OF_AGE){
-        println("Back to main page")
+    else {
+        "Back to main page"
     }
+    //println(resultText)
+    val consoleNumber = when(userAge) {
+        10 -> "Your number is 10"
+        42 -> "Your number is 42"
+        20 -> "Your number is 20"
+        else -> "Another number"
+    }
+    println(consoleNumber)
 }
 
 const val MAJORITY_OF_AGE = 18
